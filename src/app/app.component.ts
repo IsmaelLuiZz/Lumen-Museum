b9e4f2e37032d7571c1c1ef3d71d5479149ffc78
ismaelluizz <contatoismaelluiz@gmail.com>
2025-04-25 14:52:27 -0300

    melhora do header e footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./components/template/header/header.component";
+import { FooterComponent } from "./components/template/footer/footer.component";
+import { HomeComponent } from "./views/home/home.component";;
+import { ViewportScroller } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'lumen-museum';
-}
+  export class AppComponent {
+    title = 'lumen-museum';
+    constructor(private scroller: ViewportScroller) {}
+  
+    scrollTo(anchor: string) {
+      this.scroller.scrollToAnchor(anchor);
+    }
+  }
